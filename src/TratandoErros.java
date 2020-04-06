c86e66c8e3f167e4556bfbb5494875eb208c11e3
ismail younes <isma.younes1998@gmail.com>
2020-04-06 18:36:35 -0300

    atualizacao

--- src/TratandoErros.java
+++ src/TratandoErros.java
@@ -4,7 +4,7 @@ public class TratandoErros {
 	public static void main(String[] args) {
 	    System.out.println("Inicio do main");
 	    try {
-	        metodo1();
+	        metodo();
 	    } catch(ArithmeticException | NullPointerException ex) {
 	        System.out.println("Exception");
 	    }
@@ -12,16 +12,12 @@ public class TratandoErros {
 	    
 	}
 
-	private static void metodo1() {
-		System.out.println("Inicio do metodo1");
-		metodo2();
-		System.out.println("Fim do metodo1");	
-	}
-	private static void metodo2() {
+	
+	private static void metodo() {
 	    System.out.println("Inicio do metodo2");
 	    for(int i = 1; i <= 5; i++) {
 	        System.out.println(i);
-	        int a = i / 0;
+	        int a = 4 / 0;
 	        Conta riad = null;
 	        riad.deposita();
 	    }
